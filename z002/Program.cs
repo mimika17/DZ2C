bd30fa08e9a03a87ddfd94481fd7f38daacb95eb
anna <mimika021981@gmail.com>
2022-08-24 12:59:44 +0500

    DZ2

---
+++ z002/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 13: Напишите программу, которая выводит
+// третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.WriteLine("Введите число: ");
+int a1 = Convert.ToInt32(Console.ReadLine());
+void NumberThree(int num3)
+{
+    string a1lenght = Convert.ToString(num3);
+    int c = a1lenght.Length;
+    if (c > 2) Console.WriteLine(a1lenght[2]);
+    else Console.WriteLine("третьей цифры нет");
+}
+NumberThree(a1);
---
+++ z003/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, 
+// обозначающую день недели, и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+Console.WriteLine("Введите число: ");
+int day = Convert.ToInt32(Console.ReadLine());
+if (day <= 0) Console.WriteLine("Это не день недели, введите число от одного до семи");
+else if (day == 6) Console.WriteLine("Да, это выходной");
+else if (day == 7) Console.WriteLine("Да, это выходной");
+else if (day <= 5) Console.WriteLine("Нет, это не выходной");
+else if (day > 7) Console.WriteLine("Это не день недели, введите число от одного до семи");
